a7d5640c18a6849fbc9cc9f2349dea328288825b
sagar1121345 <62881150+sagar1121345@users.noreply.github.com>
2023-12-17 13:28:03 +0530

    Variables

---
+++ variables/index.js
@@ -0,0 +1,18 @@
+
+// Can't changed, once delclared
+const name = "sagar";
+
+// Can be changed
+// It have scope
+let city =  "Rajkot";
+
+
+// Can be changed
+// It have not scope
+var age = 23
+
+caste = "patel"
+
+
+
+console.table([name,city,age,caste])
